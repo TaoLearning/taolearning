ee2f4f507e7e7b7b54da4bf59f70274595fd2174
Sean Hackney <sean@taoninja.com>
2019-07-18 22:24:42 -0400

    Payment Updates

--- js/client.js
+++ js/client.js
@@ -7,6 +7,7 @@ cardElement.mount('#card-element');
 
 var cardholderName = document.getElementById('cardholder-name');
 var cardButton = document.getElementById('card-button');
+var amount = 
 
 cardButton.addEventListener('click', function(ev) {
   stripe.createPaymentMethod('card', cardElement, {
@@ -18,11 +19,14 @@ cardButton.addEventListener('click', function(ev) {
       errorElement.textContent = result.error.message;
       // Show error in payment form
     } else {
+      var myHeaders = new Headers();
       console.log("Send Payment");
       // Otherwise send paymentMethod.id to your server (see Step 2)
       fetch('https://stripedonate.azurewebsites.net/api/StripeHttpTrigger?code=/xlyHNsnNnqie7yQTDf0fVgPAGaC/D259rKok9dNWRraEIX8MhX5yg==', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: myHeaders,
+        mode: 'no-cors',
+        cache: 'default',
         body: JSON.stringify({ payment_method_id: result.paymentMethod.id })
       }).then(function(result) {
         // Handle server response (see Step 3)
@@ -33,3 +37,34 @@ cardButton.addEventListener('click', function(ev) {
     }
   });
 });
+
+
+function handleServerResponse(response) {
+  if (response.error) {
+    // Show error from server on payment form
+  } else if (response.requires_action) {
+    // Use Stripe.js to handle required card action
+    stripe.handleCardAction(
+      response.payment_intent_client_secret
+    ).then(function(result) {
+      if (result.error) {
+        // Show error in payment form
+      } else {
+        var myHeaders = new Headers();
+        // The card action has been handled
+        // The PaymentIntent can be confirmed again on the server
+        fetch('https://stripedonate.azurewebsites.net/api/StripeHttpTrigger?code=/xlyHNsnNnqie7yQTDf0fVgPAGaC/D259rKok9dNWRraEIX8MhX5yg==', {
+          method: 'POST',
+          headers: myHeaders,
+          mode: 'no-cors',
+          cache: 'default',
+          body: JSON.stringify({ payment_intent_id: result.paymentIntent.id })
+        }).then(function(confirmResult) {
+          return confirmResult.json();
+        }).then(handleServerResponse);
+      }
+    });
+  } else {
+    // Show success message
+  }
+}
