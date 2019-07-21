6b0dde9c8973d4149410b8caf52d527ce0be3504
Matthew Hackney <matthew@taoninja.com>
2019-07-21 16:21:44 -0400

    Donation Gateway Optimization

--- js/client.js
+++ js/client.js
@@ -1,72 +1,78 @@
 const stripe = Stripe('pk_test_M4lUHJesnz3f9gWbUJzdncMV');
+const fetchlink = 'https://stripedonate.azurewebsites.net/api/StripeHttpTrigger?code=/xlyHNsnNnqie7yQTDf0fVgPAGaC/D259rKok9dNWRraEIX8MhX5yg==';
 
 const elements = stripe.elements();
 const cardElement = elements.create('card');
 cardElement.mount('#card-element');
 const cardholderName = document.getElementById('cardholder-name');
 const cardButton = document.getElementById('card-button');
+var errorElement = document.getElementById('card-errors');
 
 cardButton.addEventListener('click', async (ev) => {
-  console.log("Submit Clicked");
-  var email = document.getElementById('email').value;
-  var tel = document.getElementById('tel').value;
-  var address = document.getElementById('address').value;
-  var city = document.getElementById('city').value;
-  var state = document.getElementById('state').value;
-  var donationamount = $("input[name=donation-amount]:checked").val();
-  var subscription = document.getElementById("subscription").checked;
-  var membership = document.getElementById("membership").checked;
+var email = document.getElementById('email').value;
+var tel = document.getElementById('tel').value;
+var address = document.getElementById('address').value;
+var city = document.getElementById('city').value;
+var state = document.getElementById('state').value;
+var totalamount = parseInt($("input[name=donation-amount]:checked").val());
+var subscription = document.getElementById("subscription").checked;
+var membership = document.getElementById("membership").checked;
 
+  // console.log("Submit Clicked");
   const {paymentMethod, error} =
-    await stripe.createPaymentMethod('card', cardElement, {
-      billing_details: {name: cardholderName.value}
-    });
+  await stripe.createPaymentMethod('card', cardElement, {
+    billing_details: {name: cardholderName.value}
+  });
   if (error) {
-    // console.log(result.error.message);
-    // var errorElement = document.getElementById('card-errors');
-    // errorElement.textContent = result.error.message;
+  $("#card-errors").fadeIn();
+  // console.log(error.message);
+  errorElement.textContent = error.message;
   } else {
-    // Send paymentMethod.id to your server (see Step 2)
-    var fullbody = JSON.stringify({donationamount : donationamount, subscription : subscription, email : email, tel : tel, address: address, city : city, state : state, membership : membership, payment_method_id: paymentMethod.id});
-      console.log(fullbody);
+  $("#card-errors").fadeOut();
+  // console.log("Send Payment Stringify to Server");
+  var fullbody = JSON.stringify({totalamount : totalamount, subscription : subscription, email : email, tel : tel, address: address, city : city, state : state, membership : membership, payment_method_id: paymentMethod.id});
+  console.log(fullbody);
 
-    const response = await fetch('https://stripedonate.azurewebsites.net/api/StripeHttpTrigger?code=/xlyHNsnNnqie7yQTDf0fVgPAGaC/D259rKok9dNWRraEIX8MhX5yg==', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: fullbody
-    });
+  const response = await fetch(fetchlink, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: fullbody
+  });
 
-    const json = await response.json();
+  const json = await response.json();
 
-    // Handle server response (see Step 3)
-    handleServerResponse(json);
+  handleServerResponse(json);
   }
 });
 
 
 const handleServerResponse = async (response) => {
   if (response.error) {
-    // Show error from server on payment form
-    console.log(response.error);
+    $("#card-errors").fadeIn();
+    // console.log(response.error.message);
+    errorElement.textContent = response.error.message;
   } else if (response.requires_action) {
-    // Use Stripe.js to handle the required card action
     const { error: errorAction, paymentIntent } =
-      await stripe.handleCardAction(response.payment_intent_client_secret);
+    await stripe.handleCardAction(response.payment_intent_client_secret);
 
     if (errorAction) {
-      // Show error from Stripe.js in payment form
+    // console.log(errorAction.message);
+    $("#card-errors").fadeIn();
+    errorElement.textContent = errorAction.message;
     } else {
-      // The card action has been handled
-      // The PaymentIntent can be confirmed again on the server
-      const serverResponse = await fetch('https://stripedonate.azurewebsites.net/api/StripeHttpTrigger?code=/xlyHNsnNnqie7yQTDf0fVgPAGaC/D259rKok9dNWRraEIX8MhX5yg==', {
+      // console.log("Send Payment (again) Stringify to Server");
+
+      const serverResponse = await fetch(fetchlink, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ payment_intent_id: paymentIntent.id })
       });
+      
+
       handleServerResponse(await serverResponse.json());
     }
   } else {
-    // Show success message
-    // console.log("Success Message at End");
+    // console.log("Payment Successful");
+    window.location.replace("thankyou.html?name=" + cardholderName "&amount=" + totalamount + "&membership=" + membership + "&subscription=" + subscription);
   }
 }
